2cb13431cd50998de5be9c4247e6c8ddce9419b6
IneseKo <in.kocina@gmail.com>
2021-08-30 20:55:08 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void method2() {
+        System.out.println("hey");
+        System.out.println("practicing");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        System.out.println("Hello World one more time");
+    }
+
+}
