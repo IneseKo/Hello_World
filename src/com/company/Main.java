5353709f16698fc92bee8a3ed7fe29e4d8d57ada
IneseKo <in.kocina@gmail.com>
2021-09-01 18:58:11 +0300

    New statement added.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,14 +2,10 @@ package com.company;
 
 public class Main {
 
-    public static void method2() {
-        System.out.println("hey");
-        System.out.println("practicing");
-    }
-
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("Hello World one more time");
+        System.out.println("Hello World 2nd version");
     }
 
 }
